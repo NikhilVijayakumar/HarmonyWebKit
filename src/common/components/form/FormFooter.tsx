aaf1f8055d11a33b65ff3e762e8297f53fd6aa29
Nikhil Vijayakumar <nvijayakumar@opentrends.net>
2023-11-08 18:41:13 +0530

    add form footer

---
+++ src/common/components/form/FormFooter.tsx
@@ -0,0 +1,47 @@
+//path src/common/components/form/FormFooter.tsx
+import React from 'react'
+import Grid from '@mui/material/Grid'
+import { Theme, ThemeProvider } from '@mui/material/styles'
+
+export type ThemeContextValue = {
+  darkMode: boolean
+  toggleDarkMode: () => void
+}
+
+type FormFooterProps = {
+  lightTheme: Theme
+  darkTheme: Theme
+  themeContext: ThemeContextValue
+}
+
+const FormFooter: React.FC<FormFooterProps> = ({
+  lightTheme,
+  darkTheme,
+  themeContext,
+}) => {
+  return (
+    <ThemeProvider theme={themeContext.darkMode ? darkTheme : lightTheme}>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        style={{
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          width: '100%',
+          background: themeContext.darkMode ? 'black' : 'white',
+          borderTop: '1px solid #ccc',
+          padding: '10px 0',
+          color: themeContext.darkMode ? 'white' : 'black',
+        }}
+      >
+        <p>
+          &copy; {new Date().getFullYear()} Skill Sage. All rights reserved.
+        </p>
+      </Grid>
+    </ThemeProvider>
+  )
+}
+
+export default FormFooter
